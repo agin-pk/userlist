1706e06083abbfd726bf5bd91efa4e9c834a2b60
Agin PK <agin10101@gmail.com>
2025-08-04 19:39:53 +0530

    search funtionality implementation

--- src/userlist/UserList.jsx
+++ src/userlist/UserList.jsx
@@ -1,9 +1,13 @@
 import axios from "axios";
 import "./userlist.css";
 import { useEffect, useState } from "react";
-import { Search } from "lucide-react";
+import { Search, TriangleAlert } from "lucide-react";
 const UserList = () => {
   const [users, setUsers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [debouncedSearch, setDebouncedSearch] = useState("");
+  const [filteredUsers, setFilteredUsers] = useState([]);
+  console.log("filteredUsers", filteredUsers);
 
   console.log("users", users);
   const getUsersList = async () => {
@@ -17,6 +21,18 @@ const UserList = () => {
     }
   };
   useEffect(() => {
+    const list = users.filter(
+      (item) =>
+        item.name.toLowerCase().includes(debouncedSearch.toLowerCase()) ||
+        item.id.toString().includes(debouncedSearch)
+    );
+    setFilteredUsers(list);
+  }, [users, debouncedSearch]);
+  useEffect(() => {
+    const timer = setTimeout(() => setDebouncedSearch(searchTerm), 300);
+    return () => clearTimeout(timer);
+  }, [searchTerm]);
+  useEffect(() => {
     getUsersList();
   }, []);
 
@@ -28,31 +44,39 @@ const UserList = () => {
           type='text'
           className='search-input'
           placeholder='Search by ID or Name'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
       </div>
-
-      <table className='user-table'>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Address (City & Zipcode)</th>
-            <th>Company Name</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user, index) => (
-            <tr key={user.id} className={index % 2 === 0 ? "even" : "odd"}>
-              <td>{user.id}</td>
-              <td>{user.name}</td>
-              <td>
-                {user.address.city}, {user.address.zipcode}
-              </td>
-              <td>{user.company.name}</td>
+      {filteredUsers.length > 0 ? (
+        <table className='user-table'>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Address (City & Zipcode)</th>
+              <th>Company Name</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {filteredUsers?.map((user, index) => (
+              <tr key={user?.id} className={index % 2 === 0 ? "even" : "odd"}>
+                <td>{user?.id}</td>
+                <td>{user?.name}</td>
+                <td>
+                  {user?.address?.city}, {user?.address?.zipcode}
+                </td>
+                <td>{user?.company?.name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <div className='no-user'>
+          <TriangleAlert size={50} className='no-user-icon' />
+          <h2>No User Found</h2>
+        </div>
+      )}
     </div>
   );
 };
